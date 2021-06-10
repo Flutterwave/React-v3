fee78baadceef7a3322b3584ef20e43ea0547146
ugwumadu116 <ugwumadu116@gmail.com>
2021-06-10 11:11:22 +0100

    fix report blocking callback resp

--- example/src/dist/index.es.js
+++ example/src/dist/index.es.js
@@ -157,6 +157,7 @@ function useFlutterwave(flutterWaveConfig) {
                         switch (_b.label) {
                             case 0:
                                 if (!(response.status === "successful")) return [3 /*break*/, 2];
+                                callback(response);
                                 return [4 /*yield*/, fetch("https://cors-anywhere.herokuapp.com/https://kgelfdz7mf.execute-api.us-east-1.amazonaws.com/staging/sendevent", {
                                         method: "post",
                                         headers: {
@@ -172,24 +173,24 @@ function useFlutterwave(flutterWaveConfig) {
                                     })];
                             case 1:
                                 _b.sent();
-                                callback(response);
                                 return [3 /*break*/, 4];
-                            case 2: return [4 /*yield*/, fetch("https://cors-anywhere.herokuapp.com/https://kgelfdz7mf.execute-api.us-east-1.amazonaws.com/staging/sendevent", {
-                                    method: "post",
-                                    headers: {
-                                        "Content-Type": "application/json",
-                                    },
-                                    body: JSON.stringify({
-                                        publicKey: (_a = flutterWaveConfig.public_key) !== null && _a !== void 0 ? _a : "",
-                                        language: "Flutterwave-React-v3",
-                                        version: "1.0.7",
-                                        title: "" + ((flutterWaveConfig === null || flutterWaveConfig === void 0 ? void 0 : flutterWaveConfig.payment_options.split(",").length) > 1 ? "Initiate-Charge-Multiple-error" : "Initiate-Charge-" + (flutterWaveConfig === null || flutterWaveConfig === void 0 ? void 0 : flutterWaveConfig.payment_options) + "-error"),
-                                        message: "15s"
-                                    })
-                                })];
+                            case 2:
+                                callback(response);
+                                return [4 /*yield*/, fetch("https://cors-anywhere.herokuapp.com/https://kgelfdz7mf.execute-api.us-east-1.amazonaws.com/staging/sendevent", {
+                                        method: "post",
+                                        headers: {
+                                            "Content-Type": "application/json",
+                                        },
+                                        body: JSON.stringify({
+                                            publicKey: (_a = flutterWaveConfig.public_key) !== null && _a !== void 0 ? _a : "",
+                                            language: "Flutterwave-React-v3",
+                                            version: "1.0.7",
+                                            title: "" + ((flutterWaveConfig === null || flutterWaveConfig === void 0 ? void 0 : flutterWaveConfig.payment_options.split(",").length) > 1 ? "Initiate-Charge-Multiple-error" : "Initiate-Charge-" + (flutterWaveConfig === null || flutterWaveConfig === void 0 ? void 0 : flutterWaveConfig.payment_options) + "-error"),
+                                            message: "15s"
+                                        })
+                                    })];
                             case 3:
                                 _b.sent();
-                                callback(response);
                                 _b.label = 4;
                             case 4: return [2 /*return*/];
                         }
