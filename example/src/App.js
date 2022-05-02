5a5bacbf8f484b19bda3156e1a00383d24d79ef8
Cynthia Ilojeme <cynthiaezinne1@gmail.com>
2022-05-02 17:36:36 +0100

    add support for subaccounts

--- example/src/App.js
+++ example/src/App.js
@@ -20,6 +20,30 @@ export default function App() {
       description: 'Payment for items in cart',
       logo: 'https://assets.piedpiper.com/logo.png',
     },
+
+    subaccounts: [
+      {
+        // vendor A
+        id: "RS_D87A9EE339AE28BFA2AE86041C6DE70E",
+        transaction_split_ratio: 2,
+        transaction_charge_type: "flat",
+        transaction_charge: 100,
+      },
+      {
+        // vendor B
+        id: "RS_344DD49DB5D471EF565C897ECD67CD95",
+        transaction_split_ratio: 3,
+        transaction_charge_type: "flat",
+        transaction_charge: 100,
+      },
+      {
+        // vendor C
+        id: "RS_839AC07C3450A65004A0E11B83E22CA9",
+        transaction_split_ratio: 5,
+        transaction_charge_type: "flat",
+        transaction_charge: 100,
+      },
+    ],
   };
   
   const handleFlutterPayment = useFlutterwave(config);
