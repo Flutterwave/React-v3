437032e017e728cba9406d06ae61c1a99b72b3b0
ugwumadu116 <ugwumadu116@gmail.com>
2023-03-10 17:00:47 +0100

    added test

---
+++ test/flutterwave.spec.ts
@@ -0,0 +1,53 @@
+
+/**
+ * @jest-environment jsdom
+ */
+
+
+import React, {useState} from 'react';
+import useFWScript from '../src/script';
+import useFW from '../src/useFW';
+import { renderHook } from "@testing-library/react";
+
+import '@testing-library/jest-dom'
+
+
+test('FlutterwaveModule should create', () => {
+  const {result} = renderHook(() => useFWScript())
+
+  expect(
+    result.current[0]
+  ).not.toBeTruthy();
+  
+})
+
+test('should load Flutterwave Inline script and have FlutterwaveCheckout function', () => {
+  const config = {
+    public_key: "FLWPUBK-**************************-X",
+    tx_ref: "text_ref1234",
+    amount: 10,
+    currency: 'NGN',
+    payment_options: 'card,mobilemoney,ussd',
+    customer: {
+      email: 'user@gmail.com',
+      phone_number: '08102909304',
+      name: 'test user',
+    },
+
+    customizations: {
+      title: 'My store',
+      description: 'Payment for items in cart',
+      logo: 'https://assets.piedpiper.com/logo.png',
+    },
+
+   
+  };
+  
+  const {result} = renderHook(() => useFW(config))
+
+      expect(
+      document.querySelector(`script[src="https://checkout.flutterwave.com/v3.js"]`)
+    ).not.toBeNull();
+  
+})
+
