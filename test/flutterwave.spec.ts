9e3eb2f1574cd58559f0c17bf2e7fa03e6e757c7
ChijiokeFLW <chijioke.okorji@flutterwavego.com>
2024-02-28 19:32:05 +0100

    Resolved lint errors

--- test/flutterwave.spec.ts
+++ test/flutterwave.spec.ts
@@ -1,30 +1,25 @@
-
 /**
  * @jest-environment jsdom
  */
-
-
-import React, {useState} from 'react';
 import useFWScript from '../src/script';
 import useFW from '../src/useFW';
-import { renderHook } from "@testing-library/react";
+import { renderHook } from '@testing-library/react';
 
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom';
 
 
 test('FlutterwaveModule should create', () => {
-  const {result} = renderHook(() => useFWScript())
-
+  const {result} = renderHook(() => useFWScript({}));
+  
   expect(
     result.current[0]
   ).not.toBeTruthy();
-  
-})
+});
 
-test('should load Flutterwave Inline script and have FlutterwaveCheckout function', () => {
+test('Should load Flutterwave Inline script and have FlutterwaveCheckout function', () => {
   const config = {
-    public_key: "FLWPUBK-**************************-X",
-    tx_ref: "text_ref1234",
+    public_key: 'FLWPUBK-**************************-X',
+    tx_ref: 'text_ref1234',
     amount: 10,
     currency: 'NGN',
     payment_options: 'card,mobilemoney,ussd',
@@ -33,21 +28,17 @@ test('should load Flutterwave Inline script and have FlutterwaveCheckout functio
       phone_number: '08102909304',
       name: 'test user',
     },
-
     customizations: {
       title: 'My store',
       description: 'Payment for items in cart',
       logo: 'https://assets.piedpiper.com/logo.png',
     },
-
-   
   };
   
-  const {result} = renderHook(() => useFW(config))
+  const {result} = renderHook(() => useFW(config));
 
-      expect(
-      document.querySelector(`script[src="https://checkout.flutterwave.com/v3.js"]`)
-    ).not.toBeNull();
-  
-})
+  expect(
+    document.querySelector('script[src="https://checkout.flutterwave.com/v3.js"]')
+  ).not.toBeNull();
+});
 
