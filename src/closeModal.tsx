6ac0666950a1a7287967638e69ef990f103349fe
ChijiokeFLW <chijioke.okorji@flutterwavego.com>
2024-02-28 13:04:45 +0100

    Resolved lint errors

--- src/closeModal.tsx
+++ src/closeModal.tsx
@@ -1,13 +1,12 @@
 /**
  * function to be called when you want to close payment
  */
- export default function closePaymentModal() {
+ export default function closePaymentModal(): void {
   document.getElementsByName('checkout').forEach(item => {
     item.setAttribute('style',
     'position:fixed;top:0;left:0;z-index:-1;border:none;opacity:0;pointer-events:none;width:100%;height:100%;');
     item.setAttribute('id','flwpugpaidid');
-    item.setAttribute('src','https://checkout.flutterwave.com/?')
+    item.setAttribute('src','https://checkout.flutterwave.com/?');
   document.body.style.overflow = '';
-  })
-
+  });
 }
